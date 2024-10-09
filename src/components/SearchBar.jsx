ffe1d7a59bf96c33b569023ac93103c325b2327c
killertej <tejasdeshpande29@gmail.com>
2024-10-09 17:22:26 +0530

    added deploy file

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,3 @@
-// import React from 'react';
-
-// const SearchBar = ({ searchQuery, setSearchQuery }) => {
-//   return (
-//     <input
-//       type="text"
-//       placeholder="Search transactions..."
-//       value={searchQuery}
-//       onChange={(e) => setSearchQuery(e.target.value)}
-//     />
-//   );
-// };
-
-// export default SearchBar;
-
-
 import React from 'react';
 import './SearchBar.css';
 const SearchBar = ({ searchQuery, setSearchQuery }) => {
